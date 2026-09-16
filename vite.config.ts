8e49bd9d4e10f72e73de0661e8ecb65ea743fb74
Muhammad Febrian Maulana <61956108+FebrianSTEM@users.noreply.github.com>
2026-09-16 14:54:04 +0700

    update base in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/febrian-portfolio/',
+  base: '/',
   test: {
     globals: true,
     environment: 'happy-dom',
